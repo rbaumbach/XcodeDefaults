62d3dc7f788d639b6f338be12206b5420759a548
Ryan Baumbach <github@ryan.codes>
2022-12-09 07:53:33 -0800

    Reformat Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,39 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "<ENTER NAME HERE>",
-    platforms: [.iOS(.v11)],
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         .library(
             name: "<ENTER NAME HERE>",
-            targets: ["<ENTER NAME HERE>"])
+            targets: ["<ENTER NAME HERE>"]
+        ),
+        .library(
+            name: "<ENTER ANOTHER NAME HERE>",
+            targets: ["<ENTER ANOTHER NAME HERE>"]
+        )
     ],
     targets: [
         .target(
-            name: "<ENTER NAME HERE>"),
+            name: "<ENTER NAME HERE>"
+        ),
+        .target(
+            name: "<ENTER ANOTHER NAME HERE>",
+            dependencies: ["<ENTER NAME HERE>"]
+        ),
         .testTarget(
-            name: "<ENTER NAME HERE>Specs",
-            dependencies: ["<ENTER NAME HERE>"])
+            name: "<ENTER TEST NAME HERE>",
+            dependencies: ["<ENTER NAME HERE>"]
+        ),
+        .testTarget(
+            name: "<ENTER ANOTHER TEST NAME HERE>",
+            dependencies: ["<ENTER NAME HERE>","<ENTER ANOTHER NAME HERE>"]
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
